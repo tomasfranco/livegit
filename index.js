100c3d10b401b2ab1520a06f943d1a6bdac684cd
Tomás Franco <tomas.f.carvalho@gmail.com>
2019-07-14 10:54:04 -0300

    chore: Alteração do retorno da rota

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/teste"),
   (req, res) => {
-    return res.json({ teste: "world " });
+    return res.json({ teste: "world, alterando e testanto o git" });
   };
 
 app.listen(3333);
